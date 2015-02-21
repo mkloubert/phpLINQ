c45dde244cb2ddf7e02295c36d38c2d983a9b1c5
Marcel J. Kloubert <marcel.kloubert@gmx.net>
2015-02-21 23:18:40 +0100

    - UPD: examples for intersect() method

--- examples/examples_intersect.php
+++ examples/examples_intersect.php
@@ -12,13 +12,15 @@ $pageTitle = 'intersect()';
 
 // example #1
 $examples[] = new Example();
+$examples[0]->title = 'Default behavior';
 $examples[0]->sourceCode = 'use \\System\\Linq\\Enumerable;
 	
 $seq1 = Enumerable::fromValues(1, 2, 3, 4, 5);
 $seq2 = Enumerable::fromValues(3, 2);
 $seq3 = Enumerable::fromValues(6, 7);
 $seq4 = Enumerable::createEmpty();
-
+$seq5 = Enumerable::fromValues("2", 5);
+		
 
 $showSequence = function($seq) {
 	foreach ($seq as $item) {
@@ -41,6 +43,57 @@ echo "\n";
 echo "seq1 + seq4:\n";
 $showSequence($seq1->reset()
 		           ->intersect($seq4));
+		
+echo "\n";
+		
+echo "seq1 + seq5:\n";
+$showSequence($seq1->reset()
+		           ->intersect($seq5));
+';
+
+// example #2
+$examples[] = new Example();
+$examples[1]->title = 'Custom comparer';
+$examples[1]->sourceCode = 'use \\System\\Linq\\Enumerable;
+
+$seq1 = Enumerable::fromValues(1, 2, 3, 4, 5);
+$seq2 = Enumerable::fromValues(3, 2);
+$seq3 = Enumerable::fromValues(6, 7);
+$seq4 = Enumerable::createEmpty();
+$seq5 = Enumerable::fromValues("2", 5);
+
+
+$myComparer = function($x, $y) {
+	return $x === $y;
+};
+
+$showSequence = function($seq) {
+	foreach ($seq as $item) {
+		echo "{$item}\n";
+	}
+};
+
+
+echo "seq1 + seq2:\n";
+$showSequence($seq1->intersect($seq2, $myComparer));
+
+echo "\n";
+
+echo "seq1 + seq3:\n";
+$showSequence($seq1->reset()
+		           ->intersect($seq3, $myComparer));
+
+echo "\n";
+
+echo "seq1 + seq4:\n";
+$showSequence($seq1->reset()
+		           ->intersect($seq4, $myComparer));
+		
+echo "\n";
+		
+echo "seq1 + seq5:\n";
+$showSequence($seq1->reset()
+		           ->intersect($seq5, $myComparer));
 ';
 
 
