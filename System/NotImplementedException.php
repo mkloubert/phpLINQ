7aded51ae8128cb1070b0d0cb876c6e4e835f863
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-08-04 06:06:45 +0200

    - ADD: NotImplementedException class

---
+++ System/NotImplementedException.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ *  LINQ concept for PHP
+ *  Copyright (C) 2015  Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3.0 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *    Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with this library.
+ */
+
+namespace System;
+
+
+/**
+ * Is thrown if something is not implemented yet.
+ *
+ * @package System
+ * @author Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
+ */
+class NotImplementedException extends Exception {
+}
