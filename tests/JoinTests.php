7e61577ac7fc22c45c78dd01bcfbfeb88c5a14cb
Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
2015-09-24 13:04:21 +0200

    - ADD: tests for join() method

---
+++ tests/JoinTests.php
@@ -0,0 +1,293 @@
+<?php
+
+/**********************************************************************************************************************
+ * phpLINQ (https://github.com/mkloubert/phpLINQ)                                                                     *
+ *                                                                                                                    *
+ * Copyright (c) 2015, Marcel Joachim Kloubert <marcel.kloubert@gmx.net>                                              *
+ * All rights reserved.                                                                                               *
+ *                                                                                                                    *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the   *
+ * following conditions are met:                                                                                      *
+ *                                                                                                                    *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the          *
+ *    following disclaimer.                                                                                           *
+ *                                                                                                                    *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+ *    following disclaimer in the documentation and/or other materials provided with the distribution.                *
+ *                                                                                                                    *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote    *
+ *    products derived from this software without specific prior written permission.                                  *
+ *                                                                                                                    *
+ *                                                                                                                    *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, *
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE  *
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, *
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR    *
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,  *
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE   *
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                           *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+use \System\Collections\IEnumerable;
+use \System\Linq\Enumerable;
+
+
+class Person {
+    public function __construct($name) {
+        $this->Name = $name;
+    }
+
+    public $Name;
+}
+
+class Pet {
+    public function __construct($name, Person $owner) {
+        $this->Name = $name;
+        $this->Owner = $owner;
+    }
+
+    public $Name;
+    public $Owner;
+}
+
+function personKeySelectorFunc(Person $person) {
+    return $person->Name;
+}
+
+function petKeySelectorFunc(Pet $pet) {
+    return $pet->Owner->Name;
+}
+
+function selectorFunc(Person $person, Pet $pet) {
+    return sprintf('Owner: %s; Pet: %s',
+                   $person->Name,
+                   $pet->Name);
+}
+
+class PersonKeySelectorClass {
+    public function __invoke(Person $person) {
+        return personKeySelectorFunc($person);
+    }
+}
+
+class PetKeySelectorClass {
+    public function __invoke(Pet $pet) {
+        return petKeySelectorFunc($pet);
+    }
+}
+
+class ResultSelectorClass {
+    public function __invoke(Person $person, Pet $pet) {
+        return selectorFunc($person, $pet);
+    }
+}
+
+/**
+ * @see \System\Collection\IEnumerable::join()
+ *
+ * @author Marcel Joachim Kloubert <marcel.kloubert@gmx.net>
+ */
+class JoinTests extends TestCaseBase {
+    /**
+     * Creates the data / elements for the tests.
+     *
+     * @return array The data.
+     */
+    protected function createElements() : array {
+        $persons = array(new Person("Tanja"),
+                         new Person("Marcel"),
+                         new Person("Yvonne"),
+                         new Person("Josefine"));
+
+        $pets = array(new Pet("Gina"     , $persons[1]),
+                      new Pet("Schnuffi" , $persons[1]),
+                      new Pet("Schnuffel", $persons[2]),
+                      new Pet("WauWau"   , $persons[0]),
+                      new Pet("Lulu"     , $persons[3]),
+                      new Pet("Asta"     , $persons[1]));
+
+        return [$persons, $pets];
+    }
+
+    /**
+     * Creates selectors for the tests.
+     *
+     * @return array The list of selectors.
+     */
+    protected function createSelectors() : array {
+        return [
+            [
+                function(Person $person) {
+                    return personKeySelectorFunc($person);
+                },
+                function(Pet $pet) {
+                    return petKeySelectorFunc($pet);
+                },
+                function(Person $person, Pet $pet) {
+                    return selectorFunc($person, $pet);
+                },
+            ],
+            [
+                'personKeySelectorFunc',
+                'petKeySelectorFunc',
+                'selectorFunc',
+            ],
+            [
+                '\personKeySelectorFunc',
+                '\petKeySelectorFunc',
+                '\selectorFunc',
+            ],
+            [
+                array($this, 'personKeySelectorMethod1'),
+                array($this, 'petKeySelectorMethod1'),
+                array($this, 'resultSelectorMethod1'),
+            ],
+            [
+                array(static::class, 'personKeySelectorMethod2'),
+                array(static::class, 'petKeySelectorMethod2'),
+                array(static::class, 'resultSelectorMethod2'),
+            ],
+            [
+                new PersonKeySelectorClass(),
+                new PetKeySelectorClass(),
+                new ResultSelectorClass(),
+            ],
+            [
+                '$person => $person->Name',
+                '$pet => $pet->Owner->Name',
+                '$person, $pet => sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name)',
+            ],
+            [
+                '($person) => $person->Name',
+                '($pet) => $pet->Owner->Name',
+                '($person, $pet) => sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name)',
+            ],
+            [
+                '$person => return $person->Name;',
+                '$pet => return $pet->Owner->Name;',
+                '$person, $pet => return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name);',
+            ],
+            [
+                '($person) => return $person->Name;',
+                '($pet) => return $pet->Owner->Name;',
+                '($person, $pet) => return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name);',
+            ],
+            [
+                '$person => { return $person->Name; }',
+                '$pet => { return $pet->Owner->Name; }',
+                '$person, $pet => { return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name); }',
+            ],
+            [
+                '($person) => { return $person->Name; }',
+                '($pet) => { return $pet->Owner->Name; }',
+                '($person, $pet) => { return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name); }',
+            ],
+            [
+                '$person => {
+return $person->Name;
+}',
+                '$pet => {
+return $pet->Owner->Name;
+}',
+                '$person, $pet => {
+return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name);
+}',
+            ],
+            [
+                '($person) => {
+return $person->Name;
+}',
+                '($pet) => {
+return $pet->Owner->Name;
+}',
+                '($person, $pet) => {
+return sprintf(\'Owner: %s; Pet: %s\', $person->Name, $pet->Name);
+}',
+            ],
+        ];
+    }
+
+    /**
+     * Executes tests for a person sequence.
+     *
+     * @param callable $personSeqFactory The factory that returns / produces the person sequence.
+     */
+    protected function executeForPersonSequence(callable $personSeqFactory) {
+        list($persons, $pets) = $this->createElements();
+
+        foreach ($this->createSelectors() as $selectors) {
+            list ($personKeySelector, $petKeySelector, $resultSelector) = $selectors;
+
+            $petLists   = static::sequenceListFromArray($pets);
+            $petLists[] = $pets;
+
+            foreach ($petLists as $petSeq) {
+                /* @var IEnumerable $personSeq */
+
+                $personSeq = $personSeqFactory($persons);
+
+                $joined = $personSeq->join($petSeq,
+                    $personKeySelector, $petKeySelector, $resultSelector);
+
+                $items = static::sequenceToArray($joined);
+
+                $this->assertEquals(6, count($items));
+                $this->assertEquals('Owner: Tanja; Pet: WauWau', $items[0]);
+                $this->assertEquals('Owner: Marcel; Pet: Gina', $items[1]);
+                $this->assertEquals('Owner: Marcel; Pet: Schnuffi', $items[2]);
+                $this->assertEquals('Owner: Marcel; Pet: Asta', $items[3]);
+                $this->assertEquals('Owner: Yvonne; Pet: Schnuffel', $items[4]);
+                $this->assertEquals('Owner: Josefine; Pet: Lulu', $items[5]);
+            }
+        }
+    }
+
+    public function personKeySelectorMethod1(Person $person) {
+        return personKeySelectorFunc($person);
+    }
+
+    public static function personKeySelectorMethod2(Person $person) {
+        return personKeySelectorFunc($person);
+    }
+
+    public function petKeySelectorMethod1(Pet $pet) {
+        return petKeySelectorFunc($pet);
+    }
+
+    public static function petKeySelectorMethod2(Pet $pet) {
+        return petKeySelectorFunc($pet);
+    }
+
+    public function resultSelectorMethod1(Person $person, Pet $pet) {
+        return selectorFunc($person, $pet);
+    }
+
+    public static function resultSelectorMethod2(Person $person, Pet $pet) {
+        return selectorFunc($person, $pet);
+    }
+
+    public function testArray() {
+        $this->executeForPersonSequence(function(array $persons) {
+            return Enumerable::create($persons);
+        });
+    }
+
+    public function testGenerator() {
+        $this->executeForPersonSequence(function(array $persons) {
+            $createGenerator = function() use ($persons) {
+                foreach ($persons as $p) {
+                    yield $p;
+                }
+            };
+
+            return Enumerable::create($createGenerator());
+        });
+    }
+
+    public function testIterator() {
+        $this->executeForPersonSequence(function(array $persons) {
+            return Enumerable::create(new ArrayIterator($persons));
+        });
+    }
+}
